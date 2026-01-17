1e1b8d9d5197f41c229a1348d55597e9f596eb22
orbhandari <orbhandari@connect.ust.hk>
2026-01-17 22:55:33 +0800

    implemented parser

---
+++ build/CMakeFiles/lox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lox.dir/link.d"
+  "CMakeFiles/lox.dir/main.cpp.o"
+  "CMakeFiles/lox.dir/main.cpp.o.d"
+  "bin/lox"
+  "bin/lox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
